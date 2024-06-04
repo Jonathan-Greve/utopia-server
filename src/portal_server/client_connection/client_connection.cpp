8f64a040009af387e94b36b7e8ed6ea3a53a9a5a
Jonathan Greve <jonagrev@gmail.com>
2024-06-05 02:18:12 +0800

    curly bracket initialize sm

--- src/portal_server/client_connection/client_connection.cpp
+++ src/portal_server/client_connection/client_connection.cpp
@@ -33,8 +33,8 @@ void ClientConnection::run() {
   ClientConnectionLogger client_connection_logger{client_connection_sm_context};
 
   sm<ClientConnectionStateMachine, logger<ClientConnectionLogger>>
-      client_connection_sm(io_context_, client_connection_sm_context,
-                           client_connection_logger);
+      client_connection_sm{io_context_, client_connection_sm_context,
+                           client_connection_logger};
 
   spdlog::info("Running client connection.");
   while (is_connected()) {
