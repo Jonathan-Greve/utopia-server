5dc712f116c172fc068881700fb7443b9b4c66e5
Jonathan Greve <jonagrev@gmail.com>
2024-06-07 11:56:48 +0800

    added tls packet

---
+++ include/utopia/portal_server/client_connection/packets/sts_start_tls_packet.hpp
@@ -0,0 +1,34 @@
+#pragma once
+
+#include <cstdint>
+#include <string>
+#include <vector>
+
+namespace utopia::portal::client_connection {
+class StsStartTlsPacket {
+public:
+  StsStartTlsPacket() = default;
+  StsStartTlsPacket(const std::vector<std::uint8_t> &data) noexcept;
+
+  // Returns whether the packet is valid. I.e. if it was successfully parsed
+  // Doesn't make sense to use this when sending the packet because it will
+  // always be valid when sending due to the data members having default values.
+  bool is_valid() const noexcept { return is_valid_; }
+
+  // Get the size of the packet in bytes. I.e. the number of bytes of this
+  // packet serialized. Not the size of this class in memory.
+  std::uint32_t get_packet_size() const noexcept;
+
+  std::vector<std::uint8_t> serialize() noexcept;
+
+  std::uint32_t protocol_version_major;
+  std::uint32_t protocol_version_minor;
+  std::uint32_t sequence_number;
+  std::uint32_t timeout_ms;
+  std::uint32_t xml_content_size;
+
+private:
+  std::string xml_content_;
+  bool is_valid_ = false;
+};
+} // namespace utopia::portal::client_connection
---
+++ src/portal_server/client_connection/packets/sts_start_tls_packet.cpp
@@ -0,0 +1,80 @@
+#include "utopia/utopia_pch.hpp"
+
+#include "utopia/portal_server/client_connection/packets/sts_start_tls_packet.hpp"
+
+#include "utopia/common/integers/count_digits.hpp"
+#include "utopia/common/strings/get_format_string_length_ignoring_curly_brackets.hpp"
+
+#include <pugixml.hpp>
+#include <scn/scan.h>
+#include <spdlog/spdlog.h>
+
+#include <cstdint>
+#include <string>
+#include <vector>
+
+namespace utopia::portal::client_connection {
+
+constexpr std::string_view scan_str =
+    "P /Auth/StartTls STS/{}.{}\r\ns:{};timeout={}\r\nl:{}";
+
+// before xml_content_ we have "\r\n\r\n" that we wish to skip
+constexpr std::uint32_t header_end_size = 4;
+
+StsStartTlsPacket::StsStartTlsPacket(
+    const std::vector<std::uint8_t> &data) noexcept {
+  std::string data_str(data.begin(), data.end());
+
+  auto scan_result =
+      scn::scan<std::uint32_t, std::uint32_t, std::uint32_t, std::uint32_t,
+                std::uint32_t>(data_str, scan_str);
+
+  if (!scan_result) {
+    spdlog::error("Failed to parse STS StartTls packet.");
+    is_valid_ = false;
+    return;
+  }
+
+  auto &[scan_major, scan_minor, scan_sequence_number, scan_timeout,
+         scan_xml_size] = scan_result->values();
+  protocol_version_major = scan_major;
+  protocol_version_minor = scan_minor;
+  sequence_number = scan_sequence_number;
+  timeout_ms = scan_timeout;
+  xml_content_size = scan_xml_size;
+
+  xml_content_ = std::string(scan_result->range().begin() + header_end_size,
+                             scan_result->range().end());
+
+  if (xml_content_size != xml_content_.size()) {
+    spdlog::error("XML content size does not match the expected size.");
+    is_valid_ = false;
+    return;
+  }
+
+  is_valid_ = true;
+}
+
+std::uint32_t StsStartTlsPacket::get_packet_size() const noexcept {
+  using namespace common;
+  return get_format_string_length_ignoring_curly_brackets(scan_str) +
+         count_digits(protocol_version_major) +
+         count_digits(protocol_version_minor) + count_digits(sequence_number) +
+         count_digits(timeout_ms) + count_digits(xml_content_size) +
+         xml_content_size + header_end_size;
+}
+
+std::vector<std::uint8_t> StsStartTlsPacket::serialize() noexcept {
+  xml_content_ = "";
+  xml_content_size = 0;
+
+  std::string packet_str =
+      fmt::format(scan_str, protocol_version_major, protocol_version_minor,
+                  sequence_number, timeout_ms, xml_content_size);
+
+  std::vector<std::uint8_t> packet(packet_str.begin(), packet_str.end());
+
+  return packet;
+}
+
+} // namespace utopia::portal::client_connection
