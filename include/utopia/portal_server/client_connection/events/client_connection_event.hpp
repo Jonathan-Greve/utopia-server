d3936e69180807cb3f8ccf45b6d9e56fabadd3c3
Jonathan Greve <jonagrev@gmail.com>
2024-06-09 23:02:48 +0800

    send server hello packet

--- include/utopia/portal_server/client_connection/events/client_connection_event.hpp
+++ include/utopia/portal_server/client_connection/events/client_connection_event.hpp
@@ -2,6 +2,7 @@
 
 #include "utopia/portal_server/client_connection/events/client_connection_events.hpp"
 #include "utopia/portal_server/client_connection/packets/sts/sts_connect_packet.hpp"
+#include "utopia/portal_server/client_connection/tls/events/tls_events.hpp"
 
 #include <variant>
 
@@ -9,9 +10,10 @@ namespace utopia::portal::client_connection {
 
 using ClientConnectionEvent =
     std::variant<ClientConnectionEvents::UnableToSendPacket,
-
                  ClientConnectionEvents::ReceivedValidConnectPacket,
-
                  ClientConnectionEvents::ReceivedValidStartTlsPacket,
-                 ClientConnectionEvents::SentStartTlsReplyPacket>;
+                 ClientConnectionEvents::SentStartTlsReplyPacket,
+
+                 TlsEvents::UnableToSendPacket,
+                 TlsEvents::SentServerHelloReply>;
 } // namespace utopia::portal::client_connection
--- include/utopia/portal_server/client_connection/tls/actions/send_tls_server_hello.hpp
+++ include/utopia/portal_server/client_connection/tls/actions/send_tls_server_hello.hpp
@@ -2,6 +2,7 @@
 
 #include "utopia/portal_server/client_connection/client_connection.hpp"
 #include "utopia/portal_server/client_connection/events/client_connection_event.hpp"
+#include "utopia/portal_server/client_connection/packets/tls/tls_server_hello_packet.hpp"
 #include "utopia/portal_server/client_connection/tls/events/tls_events.hpp"
 #include "utopia/portal_server/client_connection/tls/tls_context.hpp"
 
@@ -15,25 +16,19 @@ inline const auto send_tls_server_hello =
     [](asio::io_context &io,
        moodycamel::ConcurrentQueue<ClientConnectionEvent> *event_queue,
        ClientConnection &client_connection, TlsContext &context) {
-      //   StsConnectReplyPacket sts_connect_reply_packet;
-      //   sts_connect_reply_packet.protocol_version_major = 1;
-      //   sts_connect_reply_packet.protocol_version_minor = 0;
-      //   sts_connect_reply_packet.conn_type = 400;
-      //   sts_connect_reply_packet.sequence_number = context.sequence_number;
-      //   sts_connect_reply_packet.xml_content_server = 1001;
-      //   sts_connect_reply_packet.xml_content_module = 2;
-      //   sts_connect_reply_packet.xml_content_line = 1146;
+      // Send packet using default values
+      TlsServerHelloPacket tls_server_hello_packet;
 
-      //   if (!client_connection.send(sts_connect_reply_packet.serialize())) {
-      //     spdlog::error("Failed to send STS Connect Reply packet.");
-      //     event_queue->enqueue(ClientConnectionEvent{
-      //         ClientConnectionEvents::UnableToSendPacket{}});
-      //     return;
-      //   }
+      if (!client_connection.send(tls_server_hello_packet.serialize())) {
+        spdlog::error("Failed to send STS Connect Reply packet.");
+        event_queue->enqueue(
+            ClientConnectionEvent{TlsEvents::UnableToSendPacket{}});
+        return;
+      }
 
-      //   spdlog::debug("Sent STS Connect Reply packet.");
-      //   event_queue->enqueue(ClientConnectionEvent{
-      //       ClientConnectionEvents::SentConnectReplyPacket{}});
+      spdlog::debug("Sent STS Connect Reply packet.");
+      event_queue->enqueue(
+          ClientConnectionEvent{TlsEvents::SentServerHelloReply{}});
       spdlog::debug("Sending TLS Server Hello.");
     };
 
