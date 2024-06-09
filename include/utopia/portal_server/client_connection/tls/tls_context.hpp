13d06f93e13bb446620ff32d49a2b68c4055a4e1
Jonathan Greve <jonagrev@gmail.com>
2024-06-10 00:07:25 +0800

    Added server key

--- include/utopia/portal_server/client_connection/tls/tls_context.hpp
+++ include/utopia/portal_server/client_connection/tls/tls_context.hpp
@@ -9,6 +9,13 @@
 
 namespace utopia::portal::client_connection {
 
+struct ServerKey {
+  std::uint8_t prime[128];
+  std::uint8_t generator[1];
+  std::uint8_t salt[8];
+  std::uint8_t server_public[128];
+};
+
 struct TlsContext {
   TlsContext() {
     mbedtls_sha256_init(&checksum);
@@ -18,6 +25,8 @@ struct TlsContext {
   boost::circular_buffer<common::StateTransitionLogEntry> state_log{100};
 
   mbedtls_sha256_context checksum;
+
+  ServerKey server_key;
 };
 
 } // namespace utopia::portal::client_connection
