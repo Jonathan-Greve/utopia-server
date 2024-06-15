143883d77058ffa2e4c4cfc1a667f44a06f83533
Jonathan Greve <jonagrev@gmail.com>
2024-06-15 15:52:59 +0800

    Prepare to send server finished packet

---
+++ include/utopia/portal_server/client_connection/packets/tls/tls_server_finished_packet.hpp
@@ -0,0 +1,36 @@
+#pragma once
+
+#include <array>
+#include <cstdint>
+#include <string>
+#include <vector>
+
+namespace utopia::portal::client_connection {
+
+class TlsServerFinishedPacket {
+public:
+  TlsServerFinishedPacket() = default;
+  TlsServerFinishedPacket(std::vector<uint8_t> &data);
+
+  // Returns whether the packet is valid. I.e. if it was successfully parsed
+  // Doesn't make sense to use this when sending the packet because it will
+  // always be valid when sending due to the data members having default values.
+  bool is_valid() const noexcept { return is_valid_; }
+
+  // Get the size of the packet in bytes. I.e. the number of bytes of this
+  // packet serialized. Not the size of this class in memory.
+  std::uint32_t get_packet_size() const noexcept;
+
+  std::vector<uint8_t> serialize();
+
+  std::uint8_t type = 0x16;                   // byte 0
+  std::uint16_t tls_version = 0x0303;         // bytes 1-2
+  std::uint16_t size = 0x40;                  // bytes 3-4
+  std::array<std::uint8_t, 16> iv_enc;        // bytes 5-20
+  std::array<std::uint8_t, 48> encrypted_msg; // bytes 21-68
+
+private:
+  bool is_valid_ = false;
+};
+
+} // namespace utopia::portal::client_connection
---
+++ src/portal_server/client_connection/packets/tls/tls_server_finished_packet.cpp
@@ -0,0 +1,69 @@
+#pragma once
+
+#include "utopia/portal_server/client_connection/packets/tls/tls_server_finished_packet.hpp"
+
+#include "utopia/common/network/endian/endian.hpp"
+
+#include <spdlog/spdlog.h>
+
+#include <array>
+#include <cstdint>
+#include <stdexcept>
+#include <string>
+#include <vector>
+
+namespace utopia::portal::client_connection {
+
+TlsServerFinishedPacket::TlsServerFinishedPacket(std::vector<uint8_t> &data) {
+  if (data.size() < 69) {
+    spdlog::trace("Invalid TLS server key exchange packet size: {}",
+                  data.size());
+    return;
+  }
+
+  type = data[0];
+  if (type != 0x16) {
+    spdlog::trace("Invalid TLS packet type: {}", type);
+    return;
+  }
+
+  tls_version = utopia::common::be16_dec(data.data() + 1);
+  if (tls_version != 0x0303) {
+    spdlog::trace("Invalid TLS version: {}", tls_version);
+    return;
+  }
+
+  size = utopia::common::be16_dec(data.data() + 3);
+  if (size != get_packet_size() - 5) {
+    spdlog::trace("Invalid TLS packet size: {}", size);
+    return;
+  }
+
+  std::copy(data.begin() + 5, data.begin() + 21, iv_enc.begin());
+
+  std::copy(data.begin() + 21, data.begin() + 69, encrypted_msg.begin());
+
+  is_valid_ = true;
+}
+
+std::uint32_t TlsServerFinishedPacket::get_packet_size() const noexcept {
+  return 69;
+}
+
+std::vector<uint8_t> TlsServerFinishedPacket::serialize() {
+  // Preallocate the vector with the exact size needed
+  std::vector<uint8_t> packet(69);
+
+  size = static_cast<std::uint32_t>(packet.size()) - 5;
+
+  // Assign values directly using hardcoded indexes
+  packet.at(0) = type;
+  utopia::common::be16_enc(&packet.at(1), tls_version);
+  utopia::common::be16_enc(&packet.at(3), size);
+  std::copy(iv_enc.begin(), iv_enc.end(), packet.begin() + 5);
+  std::copy(encrypted_msg.begin(), encrypted_msg.end(), packet.begin() + 21);
+
+  return packet;
+}
+
+} // namespace utopia::portal::client_connection
