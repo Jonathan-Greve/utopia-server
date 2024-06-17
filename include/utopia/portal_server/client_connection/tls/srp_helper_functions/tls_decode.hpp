6ce05ae65f430ca9d501b9838e859ce4624828e4
Jonathan Greve <jonagrev@gmail.com>
2024-06-18 00:20:48 +0800

    remove commented code

--- include/utopia/portal_server/client_connection/tls/srp_helper_functions/tls_decode.hpp
+++ include/utopia/portal_server/client_connection/tls/srp_helper_functions/tls_decode.hpp
@@ -50,54 +50,6 @@ tls_decode(std::vector<uint8_t> &data, TlsContext &context) {
     return std::nullopt;
   }
 
-  //   // Compute HMAC
-  //   mbedtls_md_hmac_reset(&context.mac_dec);
-  //   mbedtls_md_hmac_update(&context.mac_dec, context.next_read_id.data(),
-  //                          context.next_read_id.size());
-
-  //   // Compute HMAC
-  //   if (mbedtls_md_hmac_update(&context.mac_dec, data.data(), 5)) {
-  //     spdlog::error("Failed to update HMAC context with modified header.");
-  //     return std::nullopt;
-  //   }
-
-  //   // Decrypted msg header (4bytes) + verify_data (12bytes)
-  //   if (mbedtls_md_hmac_update(&context.mac_dec, decrypted_msg.data(), 16)) {
-  //     spdlog::error("Failed to update HMAC context with modified header.");
-  //     return std::nullopt;
-  //   }
-
-  //   std::array<std::uint8_t, 20> calculated_hmac;
-  //   if (mbedtls_md_hmac_finish(&mac_ctx, calculated_hmac.data())) {
-  //     spdlog::error("Failed to finish HMAC calculation.");
-  //     return std::nullopt;
-  //   }
-
-  //   common::be64_enc(context.next_read_id.data(),
-  //                    common::be64_dec(context.next_read_id.data()) + 1);
-
-  //   if (!calculated_hmac) {
-  //     spdlog::error("Failed to compute HMAC.");
-  //     return std::nullopt;
-  //   }
-
-  //   // Validate the HMAC. The HMAC is 20 bytes but we don't know the exact
-  //   start
-  //   // position due to padding.
-  //   const auto found_it = std::search(decrypted_msg.begin(),
-  //   decrypted_msg.end(),
-  //                                     calculated_hmac.value().begin(),
-  //                                     calculated_hmac.value().end());
-
-  //   if (found_it == decrypted_msg.end()) {
-  //     spdlog::error("Decrypt and verify HMAC validation failed");
-  //     return std::nullopt;
-  //   }
-
-  //   // Return the decrypted message without the HMAC and padding
-  //   std::vector<uint8_t> decrypted_msg_without_hmac(decrypted_msg.begin(),
-  //                                                   found_it);
-
   return decrypted_msg;
 }
 
