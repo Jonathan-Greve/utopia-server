2f90d6ef59863653efd41779b0b91ea8cd3b91f2
Jonathan Greve <jonagrev@gmail.com>
2024-06-18 01:41:07 +0800

    created more generic tls encode function

--- include/utopia/portal_server/client_connection/tls/actions/send_tls_server_finished.hpp
+++ include/utopia/portal_server/client_connection/tls/actions/send_tls_server_finished.hpp
@@ -5,8 +5,7 @@
 #include "utopia/portal_server/client_connection/events/client_connection_event.hpp"
 #include "utopia/portal_server/client_connection/packets/tls/tls_server_finished_packet.hpp"
 #include "utopia/portal_server/client_connection/tls/events/tls_events.hpp"
-#include "utopia/portal_server/client_connection/tls/srp_helper_functions/tls_compute_handshake_finished_hmac.hpp"
-#include "utopia/portal_server/client_connection/tls/srp_helper_functions/tls_issue_next_iv.hpp"
+#include "utopia/portal_server/client_connection/tls/srp_helper_functions/tls_encode_with_hmac_and_padding.hpp"
 #include "utopia/portal_server/client_connection/tls/tls_context.hpp"
 
 #include <asio.hpp>
@@ -26,16 +25,6 @@ inline const auto send_tls_server_finished =
        ClientConnection &client_connection, TlsContext &context) {
       // Send packet using default values
       TlsServerFinishedPacket tls_server_finished_packet;
-
-      auto iv_enc_opt = tls_issue_next_iv(context);
-
-      if (!iv_enc_opt) {
-        spdlog::error("Failed to issue next IV.");
-        event_queue->enqueue(
-            ClientConnectionEvent{TlsEvents::FailedToIssueNextIV{}});
-        return;
-      }
-      tls_server_finished_packet.iv_enc = iv_enc_opt.value();
 
       // Build the server_finished data from the checksum
       constexpr std::string server_finished_label = "server finished";
@@ -78,7 +67,7 @@ inline const auto send_tls_server_finished =
 
       // Write the part of the msg to be encrypted (bytes 0 to 15. First 16
       // bytes). Header + verify data
-      std::vector<std::uint8_t> msg_to_encrypt(48);
+      std::vector<std::uint8_t> msg_to_encrypt(16);
       msg_to_encrypt[0] = 0x14;
       msg_to_encrypt[1] = 0x00;
       msg_to_encrypt[2] = 0x00;
@@ -86,33 +75,20 @@ inline const auto send_tls_server_finished =
       std::copy(context.server_finished.begin(), context.server_finished.end(),
                 msg_to_encrypt.begin() + 4);
 
-      // Compute the HMAC (bytes 16 to 35. Next 20 bytes)
-      const auto calculated_hmac = tls_compute_handshake_finished_hmac(
-          context.next_write_id, tls_server_finished_packet.serialize(),
-          msg_to_encrypt, context, context.mac_enc, 0x10);
+      auto encrypted_msg = tls_encode_with_hmac_and_padding(
+          msg_to_encrypt, tls_server_finished_packet.serialize(), context);
 
-      if (!calculated_hmac) {
-        spdlog::error("Failed to compute HMAC.");
+      if (!encrypted_msg) {
+        spdlog::error("Failed to encrypt message.");
         event_queue->enqueue(
-            ClientConnectionEvent{TlsEvents::HmacComputationFailed{}});
+            ClientConnectionEvent{TlsEvents::UnableToSendPacket{}});
+        return;
       }
 
-      // Copy the calculated HMAC into the msg to be encrypted.
-      std::copy(calculated_hmac.value().begin(), calculated_hmac.value().end(),
-                msg_to_encrypt.begin() + 16);
-
-      // Pad to 48 bytes using PKCS7 padding. But the padded value is `num bytes
-      // - 1` rather than just `num bytes`.
-      constexpr std::uint8_t num_bytes_to_pad = 48 - 36;
-      constexpr std::uint8_t padding_byte = num_bytes_to_pad - 1;
-      for (std::uint8_t i = 0; i < num_bytes_to_pad; i++) {
-        msg_to_encrypt[i + 36] = padding_byte;
-      }
-
-      // encrypt the msg
-      mbedtls_aes_crypt_cbc(&context.cipher_enc, MBEDTLS_AES_ENCRYPT, 0x30,
-                            iv_enc_opt.value().data(), msg_to_encrypt.data(),
-                            tls_server_finished_packet.encrypted_msg.data());
+      assert(encrypted_msg.value().size() == 48);
+      std::copy(encrypted_msg.value().begin(), encrypted_msg.value().end(),
+                tls_server_finished_packet.encrypted_msg.begin());
+      tls_server_finished_packet.iv_enc = context.iv_enc;
 
       // Send the packet
       if (!client_connection.send(tls_server_finished_packet.serialize())) {
@@ -121,11 +97,6 @@ inline const auto send_tls_server_finished =
             ClientConnectionEvent{TlsEvents::UnableToSendPacket{}});
         return;
       }
-
-      // Increment context.next_write_id by 1 (in big endian) to prepare for the
-      // next client message
-      common::be64_enc(context.next_write_id.data(),
-                       common::be64_dec(context.next_write_id.data()) + 1);
 
       event_queue->enqueue(
           ClientConnectionEvent{TlsEvents::SentServerFinishedPacket{}});
--- include/utopia/portal_server/client_connection/tls/srp_helper_functions/tls_encode_with_hmac_and_padding.hpp
+++ include/utopia/portal_server/client_connection/tls/srp_helper_functions/tls_encode_with_hmac_and_padding.hpp
@@ -20,37 +20,62 @@ inline std::optional<std::vector<uint8_t>>
 tls_encode_with_hmac_and_padding(std::vector<uint8_t> &msg,
                                  std::vector<std::uint8_t> tls_header,
                                  TlsContext &context) {
-  assert(tls_header.size() == 5);
+  assert(tls_header.size() >= 5);
 
   auto iv_enc_opt = tls_issue_next_iv(context);
 
   if (!iv_enc_opt) {
     spdlog::error("Failed to issue next IV.");
-    return std::std::nullopt;
+    return std::nullopt;
   }
   assert(iv_enc_opt.value() == context.iv_enc);
 
   const auto calculated_hmac = tls_compute_handshake_finished_hmac(
       context.next_write_id, tls_header, msg, context, context.mac_enc,
-      msg.size());
+      static_cast<std::uint32_t>(msg.size()));
 
   if (!calculated_hmac) {
     spdlog::error("Failed to compute HMAC.");
     return std::nullopt;
   }
 
-  std::vector<std::uint8_t> msg_to_encrypt();
-  msg_to_encrypt.insert(msg_to_encrypt.end(), tls_header.begin(),
-                        tls_header.end());
+  std::vector<std::uint8_t> msg_to_encrypt;
   msg_to_encrypt.insert(msg_to_encrypt.end(), msg.begin(), msg.end());
   msg_to_encrypt.insert(msg_to_encrypt.end(), calculated_hmac.value().begin(),
                         calculated_hmac.value().end());
 
+  // Write the padding with the modified PKCS7.
+  // We use AES, so the block size is 16, we pad to this value.
+  constexpr size_t aes_block_size = 16;
+
+  // We compute the next alignment size. If the buffer is already
+  // aligned, the aligned size will be the same as the size. This
+  // is incorrect, because it could be ambiguous whether there is
+  // a padding or not.
+  const size_t size = msg_to_encrypt.size();
+  size_t aligned = (size + (aes_block_size - 1)) & ~(aes_block_size - 1);
+
+  // When the buffer was already aligned, we simply pad it with
+  // an extra complete block.
+  if (size == aligned)
+    aligned = size + aes_block_size;
+
+  assert(size < aligned);
+  assert((aligned - size) <= aes_block_size);
+
+  // PKCS7 padding value should be the number of bytes padded, but
+  // portal use a custom one similar, but the padded value is the
+  // number of bytes - 1.
+  const auto pad_val = static_cast<uint8_t>(aligned - size - 1);
+
+  for (uint8_t i = 0; i < (pad_val + 1); ++i)
+    msg_to_encrypt.push_back(pad_val);
+
   std::vector<std::uint8_t> encrypted_msg(msg_to_encrypt.size());
   if (mbedtls_aes_crypt_cbc(&context.cipher_enc, MBEDTLS_AES_ENCRYPT,
-                            msg_to_encrypt.size(), iv_enc.data(),
+                            msg_to_encrypt.size(), iv_enc_opt.value().data(),
                             msg_to_encrypt.data(), encrypted_msg.data())) {
-    spdlog::error("Failed to decrypt message.");
+    spdlog::error("Failed to encrypt message.");
     return std::nullopt;
   }
 
